0c06cb88184cf83986b1da2fce2633d7a61d9497
KeerthiMeghana23 <Meghanakeerthi2823@gmail.com>
2025-06-20 12:34:38 +0530

    Create basics.java

---
+++ basics.java
@@ -0,0 +1,101 @@
+/* 1.class Basics
+{
+public void name()
+{
+System.out.println("Keerthi Meghana");
+}
+public static void main(String[] args)
+{	    
+Basics b=new Basics();
+b.name();
+}
+}
+Output:
+Keerthi Meghana */
+  
+
+/* 2.public class Basics
+{
+public static void main(String[] args)
+{
+System.out.println("K.Meghana"); 
+}
+}
+ 
+Output:
+K.Meghana */
+
+/* 3.class Basics
+{
+/**void name()
+{
+System.out.println("K.Meghana");
+}*/
+/*public static void main(String[] args)
+{
+/* Basics b=new Basics();
+b.name();*/
+/*System.out.println("Chandana");
+/*System.out.println("Meghana");
+}
+}
+ 
+Output:
+Meghana */
+
+/* 4.class Basics
+{
+    public static void main(String[] args)
+    {
+        int a=23;
+        boolean b=false;
+        char c='M';
+        float d= 1.01f;   
+        double e= 89.99;
+        
+        System.out.println("Integer value: "+a);
+        System.out.println("Boolean value: "+b);
+        System.out.println("Character value: "+c);
+        System.out.println("Float value: " +d);
+        System.out.println("Double value: " +e);
+    }
+}
+Output:
+Integer value: 23
+Boolean value: false
+Character value: M
+Float value: 1.01
+Double value: 89.99 */
+
+/* 5.public class Basics 
+{
+int x = 10;
+public void local() {
+int x = 20;
+System.out.println("Local variable : " + x);   
+}    
+public static void main(String[] args) 
+{
+Basics b = new Basics();
+b.local();
+System.out.println("Global Variable: "+b.x);
+}
+}
+Output:
+Local variable : 20
+Global Variable: 10 */
+
+/* 6.class Basics
+{
+public void name()
+{
+System.out.println("K.MEGHANA");
+}
+public static void main(String[] args) 
+{
+Basics b=new Basics();
+b.name();
+}
+}
+Output:
+K.MEGHANA */
